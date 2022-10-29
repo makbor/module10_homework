600584d99adf5be8a23a7c4e1630f5fe0a573d00
makbor <mak.zmicer@gmail.com>
2022-10-29 23:01:53 +0300

    Решение задания 5 методом forEach

--- JS/ex5.js
+++ JS/ex5.js
@@ -1,6 +1,12 @@
-let arr = [1, 2, 54, 10];
+let arr = [1, 2, 54, 10, 23, "sad", null];
 
-console.log("Количество элементов массива – " + arr.length)
-for (let i = 0; i < arr.length; i++) {
-  console.log(arr[i]);
-}
+console.log("Количество элементов массива – " + arr.length);
+arr.forEach(element => console.log(element));
+
+
+// let arr = [1, 2, 54, 10];
+
+// console.log("Количество элементов массива – " + arr.length)
+// for (let i = 0; i < arr.length; i++) {
+//   console.log(arr[i]);
+// }
